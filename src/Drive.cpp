1401fc48b71dac8cd242c236c28e4c7623867b49
Tsubasa1001 <a0983379890@gmail.com>
2025-01-19 11:57:00 +0800

    20250119

--- src/Drive.cpp
+++ src/Drive.cpp
@@ -12,21 +12,25 @@ void Stop(brakeType type = brakeType::brake) // 使底盤停止
     LeftMotor.stop(type);
     RightMotor.stop(type);
 }
-float AvgMotorPosition()
+float AvgMotorPosition() // 底盤馬達平均角度
 {
     return (abs(LeftMotor.position(deg)) + abs(RightMotor.position(deg))) / 2;
 }
-void ResetPosition()
+void ResetPosition() // 重置底盤角度
 {
     LeftMotor.resetPosition();
     RightMotor.resetPosition();
+    wait(20, msec);
 }
-void move()
+void move(float Distance /* 輸入位移距離(cm)*/)
 {
-
+    float enc = Cm2Enc(Distance); // 將移動距離轉換為編碼器角度
+    // float currentPosition = AvgMotorPosition(); // 取得目前編碼器位置
+    ResetPosition(); // 重置目前編碼器角度
     On(100, 100);
-    while (true)
+    while (AvgMotorPosition() < enc)
     {
         wait(20, msec);
     }
+    Stop();
 }
